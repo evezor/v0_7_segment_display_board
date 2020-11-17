64805fe684d22af62f9b84a75e2b2c596ee6b345
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2020-11-16 22:20:49 -0600

    Update simple_testcode.py

--- examples/simple_testcode.py
+++ examples/simple_testcode.py
@@ -5,7 +5,7 @@ from pyb import CAN, ADC
 import utime
 
 
-print("starting 7 segment display board test")
+print(" 7-Segment Display board test")
 print("v1.0")
 print("initializing")
 can = CAN(1, CAN.NORMAL)
@@ -15,6 +15,8 @@ can.setfilter(0, CAN.LIST16, 0, (123, 124, 125, 126))
 #Setup Pins
 hbt_led = Pin("D13", Pin.OUT)
 func_butt = Pin("D5", Pin.IN, Pin.PULL_UP) 
+can_wakeup = Pin("D6", Pin.OUT)
+can_wakeup.value(0)
 
 DIGITS_1 = ["A0", "A1", "A2", "A3"]
 DIGITS_2 = ["E13", "E14", "E15", "E17"]
@@ -24,11 +26,11 @@ SEGMENTS_2 = ["E11", "E10", "SD_DETECT", "E8", "E7", "E6", "E5", "E12"]
 
 for i in range(len(DIGITS_1)):
     DIGITS_1[i] = Pin(DIGITS_1[i], Pin.OUT)
-    DIGITS_1[i].value(1)  #Be sure to write these pins HIGH during initilization
+    DIGITS_1[i].value(1)    #Be sure to write these pins HIGH during initilization
     
 for i in range(len(DIGITS_2)):
     DIGITS_2[i] = Pin(DIGITS_2[i], Pin.OUT)
-    DIGITS_2[i].value(1)  #Be sure to write these pins HIGH during initilization
+    DIGITS_2[i].value(1)    #Be sure to write these pins HIGH during initilization
 
 for i in range(len(SEGMENTS_1)):
     SEGMENTS_1[i] = Pin(SEGMENTS_1[i], Pin.OUT)
@@ -87,7 +89,7 @@ def segments_chase():
         DIGITS_1[i].value(0)
         for j in range(len(SEGMENTS_1)):
             SEGMENTS_1[j].value(1)
-            utime.sleep_ms(150)
+            utime.sleep_ms(50)
             SEGMENTS_1[j].value(0)
         DIGITS_1[i].value(1)
     print("Display 2 chase sequence")
@@ -95,7 +97,7 @@ def segments_chase():
         DIGITS_2[i].value(0)
         for j in range(len(SEGMENTS_2)):
             SEGMENTS_2[j].value(1)
-            utime.sleep_ms(150)
+            utime.sleep_ms(50)
             SEGMENTS_2[j].value(0)
         DIGITS_2[i].value(1)
       
@@ -103,8 +105,10 @@ while True:
     chk_hbt()
     if not (func_butt.value()):
         print("function button")
+        send()
         segments_chase()
         utime.sleep_ms(200)
-        
-     if(can.any(0)):
+    
+    if(can.any(0)):
         get()
+        
